535fce2a4bfb0b82e4ebc13d735755e5b4088f96
nesimer <nicolas.remise@darva.com>
2024-12-03 11:43:03 +0100

    feat(3/12): add day 3 challenge part two for ⭐⭐ star

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ All my implementations of [Avent of Code days 2024](https://adventofcode.com/202
 
 - [1/12](days/1/main.ts) - ⭐⭐ - compute distance and similarity between two number lists
 - [2/12](days/2/main.ts) - ⭐⭐ - count safe or dampened safe report in a list of them (a report is a number list)
-- [3/12](days/3/main.ts) - ⭐ - parse a text file to extract well-formatted multiplication operations and sum their results
+- [3/12](days/3/main.ts) - ⭐⭐ - parse a text file to extract well-formatted multiplication operations and sum their results (can have do/don't rules)
 
 ## Commands
 
--- days/3/main.ts
+++ days/3/main.ts
@@ -15,40 +15,50 @@ function loadListsFile() {
 
 /**
  * A tuple representing a captured multiplication operation.
- * The first and second elements are the captured numbers.
+ * The first and second elements are the captured numbers,
  * The third element is the full match string,
  */
 type CapturedMul = [number, number, string];
 
-/**
- * Parses the input data to extract all multiplication operations.
- *
- * @param {string} data - The input data containing multiplication operations.
- * @returns {CapturedMul[]} An array of captured multiplication operations.
- */
-function parseData(data: string): CapturedMul[] {
-  const regex = /mul\((\d+),(\d+)\)/gm;
-  return data
-    .matchAll(regex)
-    .toArray()
-    .map(([captured, firstGroup, secondGroup]) => [
-      +firstGroup,
-      +secondGroup,
-      captured,
-    ]);
+function parse(data: string, regex: RegExp): IteratorObject<CapturedMul> {
+  return data.matchAll(regex).map((m) => [+m[1], +m[2], m[0]] as CapturedMul);
 }
 
 /**
- * Computes the sum of all multiplication operations in the input data.
+ * Computes the sum of all parsed multiplication operations in the input data.
  *
  * @param {string} data - The input data containing multiplication operations.
  * @returns {number} The sum of all multiplications.
  */
 function computeSumMul(data: string): number {
-  return parseData(data).reduce(
-    (acc, [first, second]) => acc + first * second,
-    0
-  );
+  const regex = /mul\((\d+),(\d+)\)/g;
+  return parse(data, regex).reduce((a, [gr1, gr2]) => (a || 0) + gr1 * gr2, 0);
+}
+
+/**
+ * Computes the sum of all parsed multiplication operations in the input data depending on do/don't rules.
+ *
+ * @param {string} data - The input data containing multiplication operations.
+ * @returns {number} The sum of all multiplications.
+ */
+function computeWantedSumMul(data: string): number {
+  const regex = /mul\((\d+),(\d+)\)|(do\(\)|don\'t\(\))/g;
+  let active = true;
+  return parse(data, regex).reduce((a, [gr1, gr2, captured]) => {
+    switch (captured) {
+      case "do()": {
+        active = true;
+        return a;
+      }
+      case "don't()": {
+        active = false;
+        return a;
+      }
+      default: {
+        return active ? a + gr1 * gr2 : a;
+      }
+    }
+  }, 0);
 }
 
 export default async function main() {
@@ -56,5 +66,10 @@ export default async function main() {
   console.log(
     `The sum of all the multiplications is: ${computeSumMul(corruptedData)}`
   );
-  console.log("⭐");
+  console.log(
+    `The sum of all the wanted multiplications is: ${computeWantedSumMul(
+      corruptedData
+    )}`
+  );
+  console.log("⭐⭐");
 }
