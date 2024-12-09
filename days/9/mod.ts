48f0a509a6e5c7f779ee5f318965ae157133a42a
nesimer <nicolas.remise@darva.com>
2024-12-09 11:37:29 +0100

    feat(9/12): add part 2 to get ⭐⭐

--- days/9/mod.ts
+++ days/9/mod.ts
@@ -55,8 +55,10 @@ function moveBlocksToLeft(diskMap: string[]): string[] {
 function computeChecksum(diskMap: string[]): number {
   let i = 0;
   let total = 0;
-  while (diskMap[i] !== ".") {
-    total += i * +diskMap[i];
+  while (i < diskMap.length) {
+    if (diskMap[i] !== ".") {
+      total += i * +diskMap[i];
+    }
     i++;
   }
   return total;
@@ -69,14 +71,94 @@ function computeChecksum(diskMap: string[]): number {
  * @param {string} diskMapData - The input data containing the disk map.
  * @returns {number} The computed checksum of the arranged disk map.
  */
-function sortAndChecksumDiskMap(diskMapData: string): number {
+function orderByBlockAndChecksumDiskMap(diskMapData: string): number {
   const diskMap = parseDiskMap(diskMapData);
   const arrangedDiskMap = moveBlocksToLeft(diskMap);
   return computeChecksum(arrangedDiskMap);
 }
 
+/**
+ * Moves whole files to the leftmost span of free space blocks that could fit the file.
+ * Attempts to move each file exactly once in order of decreasing file ID number.
+ *
+ * @param {string[]} diskMap - The initial disk map containing file blocks and free space blocks.
+ * @returns {string[]} The final disk map with no gaps between file blocks.
+ */
+function moveFilesToLeft(diskMap: string[]): string[] {
+  let arrangedDiskMap = [...diskMap];
+  const fileIdQuantity = arrangedDiskMap
+    .filter((char) => char !== ".")
+    .reduce<Map<number, number>>((acc, curr) => {
+      acc.set(+curr, (acc.get(+curr) || 0) + 1);
+      return acc;
+    }, new Map());
+
+  let i = arrangedDiskMap.length - 1;
+  while (i >= 0) {
+    const char = arrangedDiskMap[i];
+    if (char === ".") {
+      i--;
+      continue;
+    }
+    const totalToMove = fileIdQuantity.get(+char) ?? 0;
+    if (totalToMove > 0) {
+      let dotCount = 0;
+      for (let j = 0; j < arrangedDiskMap.length; j++) {
+        if (j >= i) {
+          break;
+        }
+
+        if (arrangedDiskMap[j] === ".") {
+          dotCount++;
+        } else {
+          if (dotCount < totalToMove) {
+            dotCount = 0;
+          } else {
+            arrangedDiskMap = [
+              ...arrangedDiskMap.slice(0, j - dotCount),
+              ...new Array(totalToMove).fill(arrangedDiskMap[i]),
+              ...arrangedDiskMap.slice(
+                j - dotCount + totalToMove,
+                i - totalToMove + 1
+              ),
+              ...new Array(totalToMove).fill("."),
+              ...arrangedDiskMap.slice(i + 1),
+            ];
+            break;
+          }
+        }
+      }
+    }
+    i -= totalToMove;
+  }
+
+  return arrangedDiskMap;
+}
+
+/**
+ * Parses the input data, arranges the disk map to remove gaps between file blocks,
+ * and computes the checksum of the arranged disk map.
+ *
+ * @param {string} diskMapData - The input data containing the disk map.
+ * @returns {number} The computed checksum of the arranged disk map.
+ */
+function orderByFileAndChecksumDiskMap(diskMapData: string): number {
+  const diskMap = parseDiskMap(diskMapData);
+  const arrangedDiskMap = moveFilesToLeft(diskMap);
+  return computeChecksum(arrangedDiskMap);
+}
+
 export default async function main() {
   const data = await loadFile(9);
-  console.log(`Checksum of arranged disk map: ${sortAndChecksumDiskMap(data)}`);
-  console.log(`⭐`);
+  console.log(
+    `Checksum of arranged block-to-left disk map: ${orderByBlockAndChecksumDiskMap(
+      data
+    )}`
+  );
+  console.log(
+    `Checksum of arranged by file-to-left disk map: ${orderByFileAndChecksumDiskMap(
+      data
+    )}`
+  );
+  console.log(`⭐⭐`);
 }
