435147e24c999203ae7756ccfe220e04ee3da7b3
nesimer <nicolas.remise@darva.com>
2024-12-10 09:12:52 +0100

    feat(10/12): add part 2 to get ⭐⭐

--- days/10/mod.ts
+++ days/10/mod.ts
@@ -63,7 +63,7 @@ const directions = [
  * @param {number} height - The current height value.
  * @param {Set<string>} fullTrails - A set of unique 9-height positions reachable from the trailhead.
  */
-function searchHikingTrail(
+function searchHikingTrails(
   map: number[][],
   point: Point,
   height: number,
@@ -83,13 +83,47 @@ function searchHikingTrail(
       isWithinBounds([row, col], map.length, map[0].length) &&
       map[row][col] === expectedHeight
     ) {
-      searchHikingTrail(map, [row, col], expectedHeight, fullTrails);
+      searchHikingTrails(map, [row, col], expectedHeight, fullTrails);
     }
   }
 }
 
 /**
- * Calculates the total score of all trailheads that have complete hiking trails in the topographic map.
+ * Performs a recursive depth-first search (DFS) to find all distinct hiking trails from a given trailhead.
+ *
+ * @param {number[][]} grid - The topographic map as a 2D array.
+ * @param {Point} point - The current point in the map.
+ * @param {number} height - The current height value.
+ * @returns {number} The number of distinct hiking trails from the trailhead.
+ */
+function countDistinctTrails(
+  grid: number[][],
+  point: Point,
+  height: number
+): number {
+  if (height === 9) {
+    return 1;
+  }
+
+  let count = 0;
+  for (const [dx, dy] of directions) {
+    const row = point[0] + dx;
+    const col = point[1] + dy;
+    const expectedHeight = height + 1;
+
+    if (
+      isWithinBounds([row, col], grid.length, grid[0].length) &&
+      grid[row][col] === expectedHeight
+    ) {
+      count += countDistinctTrails(grid, [row, col], expectedHeight);
+    }
+  }
+
+  return count;
+}
+
+/**
+ * Calculates the total score of all trailheads (total of hiking trails) in the topographic map.
  *
  * @param {string} data - The input data containing the topographic map.
  * @returns {number} The total score of all trailheads.
@@ -102,16 +136,40 @@ function getTrailheadsScore(data: string): number {
 
   for (const [row, col] of trailheads) {
     const fullTrails = new Set<string>();
-    searchHikingTrail(map, [row, col], 0, fullTrails);
+    searchHikingTrails(map, [row, col], 0, fullTrails);
     score += fullTrails.size;
   }
 
   return score;
 }
 
+/**
+ * Calculates the total rating of all trailheads (total of distinct) in the topographic map.
+ *
+ * @param {string} data - The input data containing the topographic map.
+ * @returns {number} The total rating of all trailheads.
+ */
+function getTrailheadRatingsScore(data: string): number {
+  const map = toMap(data);
+  const trailheads = retrieveTrailheads(map);
+
+  let ratingScore = 0;
+
+  for (const [row, col] of trailheads) {
+    ratingScore += countDistinctTrails(map, [row, col], 0);
+  }
+
+  return ratingScore;
+}
+
 export default async function main() {
   const data = await loadFile(10);
 
-  console.log(`Trailhead score: ${getTrailheadsScore(data)}`);
-  console.log("⭐");
+  console.log(`Total of hiking trails: ${getTrailheadsScore(data)}`);
+  console.log(
+    `Total of distinct hiking trails (as rating score): ${getTrailheadRatingsScore(
+      data
+    )}`
+  );
+  console.log("⭐⭐");
 }
